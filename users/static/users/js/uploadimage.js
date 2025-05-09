d1531c9a1055cb006a0e11f37739254ef3b81dc1
YuEunice <110688130+YuEunice@users.noreply.github.com>
2025-05-09 20:10:38 +0800

    refine the upload image page

--- users/static/users/js/uploadimage.js
+++ users/static/users/js/uploadimage.js
@@ -9,13 +9,16 @@ function uploadImage() {
     
     fetch('/post-upload-images/', {
         method: 'POST',
-        body: formData
+        body: formData,
+        headers: {
+            'X-CSRFToken': '{{ csrf_token }}'  // Include CSRF token
+        }
     })
     .then(response => response.json())
     .then(data => {
         if (data.success) {
             alert('Image uploaded successfully!');
-            window.location.href = data.redirect_url;  // redirect to loading page
+            window.location.href = data.redirect_url;  // redirect on success
         } else {
             alert('Error: ' + data.error);
         }
@@ -24,3 +27,4 @@ function uploadImage() {
         console.error('Error:', error);
     });
 }
+
